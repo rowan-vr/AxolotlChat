4f5984dc990dd02f54e354b41cb95527c7bbb862
Tippie <rowan@tippie.me>
2021-10-21 23:19:07 +0200

    Added addSever screen and storage and added windows support

---
+++ lib/screens/servers.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../storage.dart';
+
+class ServersScreen extends StatefulWidget {
+  const ServersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _NoServerState();
+}
+
+class _NoServerState extends State<ServersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("My Servers")),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              const Text("You don't have any servers added."),
+              FittedBox(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const AddServer()));
+                      },
+                      child: const Center(child: Text("Add server"))))
+            ])
+            /*ConstrainedBox(constraints: const BoxConstraints(minHeight: 100,maxHeight: 100),
+          child: Container(color: Colors.grey[200],
+              child: const Center(child: Text("You don't have any servers added! Add one now."))
+          )),*/
+
+            ));
+  }
+}
+
+class AddServer extends StatefulWidget {
+  const AddServer({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AddServerState();
+}
+
+class _AddServerState extends State<AddServer> {
+  final nameController = TextEditingController();
+  final addressController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    addressController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Add server")),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          double horizontal = constraints.maxWidth * 0.05;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 0, bottom: 16, left: horizontal, right: horizontal),
+                child: TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Example Server',
+                      labelText: "Server name"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 0, bottom: 16, left: horizontal, right: horizontal),
+                child: TextField(
+                  controller: addressController,
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Server address',
+                      hintText: 'example.server.net'),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(
+                      top: 0, bottom: 16, left: horizontal, right: horizontal),
+                  child: ElevatedButton.icon(
+                      onPressed: () async {
+                        dynamic dialog;
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              dialog = context;
+                              return AlertDialog(
+                                  title: const Text('Saving server...'),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const <Widget>[
+                                      Center(
+                                          child: SpinKitDualRing(
+                                              color: Colors.green, size: 50.0))
+                                    ],
+                                  ));
+                            });
+                        await Storage.addServer(
+                            nameController.text, addressController.text);
+                        Navigator.pop(dialog);
+                        await showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              dialog = context;
+                              return AlertDialog(
+                                title: const Text('Server saved!'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const <Widget>[
+                                    Text(
+                                        "The server has been added to the server list."),
+                                  ],
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Close'),
+                                  ),
+                                ],
+                              );
+                            });
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.add),
+                      label: const Text("Add server")))
+            ],
+          );
+        }));
+  }
+}
---
+++ lib/storage.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class Storage {
+  static Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  static Future<File> get _serverList async {
+    final path = await _localPath;
+    return File('$path/servers.json');
+  }
+
+  static Future<Map> get savedServers async {
+    File file = await _serverList;
+    if (file.existsSync()) {
+      String content = await file.readAsString();
+      Map<String, dynamic> servers = jsonDecode(content);
+      return servers;
+    }
+    return {};
+  }
+
+  static Future addServer(String name, String address) async {
+    File file = await _serverList;
+    Map<String, dynamic> servers = {};
+    if (file.existsSync()) {
+      String content = await file.readAsString();
+      servers = jsonDecode(content);
+    } else {
+      file.createSync();
+    }
+    servers[name] = address;
+    file.writeAsString(jsonEncode(servers));
+    return;
+  }
+}
